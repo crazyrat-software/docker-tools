998e119b6df72a19c9df6b0fe30b70014bb704a3
Tomi <tomi@crazyrat.pl>
2016-12-02 14:02:46 +0100

    First commit

---
+++ ssl-scanner/include/info.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Container info"
+
+echo "[-] testssl.sh"
+testssl.sh --version
+
+echo "[-] sslscan"
+sslscan --version
+
+echo "[-] sslyze"
+sslyze --version
---
+++ ssl-scanner/include/testssl.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+TESTSSL_INSTALL_DIR=/opt/testssl.sh/ /opt/testssl.sh/testssl.sh "$@"
